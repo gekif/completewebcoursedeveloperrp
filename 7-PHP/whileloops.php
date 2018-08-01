5cd97d8381f0b93689ad2541cac6b285064cc57f
gekif <dzulfikar.maulana@gmail.com>
2018-08-01 14:07:31 +0700

    whileloops

---
+++ 7-PHP/whileloops.php
@@ -0,0 +1,28 @@
+<?php
+
+//$i = 1;
+//$i = 5;
+
+//while ($i <= 10) {
+//while ($i <= 50) {
+//    echo "$i. hello world<br>";
+////    $i++;
+//    $i += 5;
+//}
+
+//$i = 1;
+//
+//while ($i <= 10) {
+//    $j = $i * 5;
+//    echo "$j"."<br>";
+//    $i++;
+//}
+
+
+$family = ['adi', 'wisnu', 'arki'];
+$i = 0;
+
+while ($i < sizeof($family)) {
+    echo $family[$i] . "<br>";
+    $i++;
+}
