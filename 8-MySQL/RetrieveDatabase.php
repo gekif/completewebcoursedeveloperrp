063a933d9fe1b61ec01a36ab8435d530b3f580bc
gekif <dzulfikar.maulana@gmail.com>
2018-08-02 11:20:15 +0700

    Retrieve DB

--- 8-MySQL/RetrieveDatabase.php
+++ 8-MySQL/RetrieveDatabase.php
@@ -4,7 +4,16 @@ include('MySQLConnect.php');
 //echo mysqli_connect_errno();
 
 if (mysqli_connect_errno()) {
-    echo 'not connected';
-} else {
-    echo 'connected';
+    die('not connected');
+}
+
+$query = "SELECT * FROM users";
+
+//if (mysqli_query($conn, $query)) {
+if ($result = mysqli_query($conn, $query)) {
+//    echo "Query is succesfull";
+    $row = mysqli_fetch_array($result);
+//    print_r($row);
+//    print_r($row);
+    echo 'Your email is ' . $row['email'] . ' and your password is \'' . $row['password'] . '\'';
 }
