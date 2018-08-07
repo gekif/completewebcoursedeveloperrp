821e39274e9fd22e6f7cd86863e6d4a086b157f2
gekif <dzulfikar.maulana@gmail.com>
2018-08-07 08:26:38 +0700

    Variables and Array

--- 12-Python/VariableAndArray.py
+++ 12-Python/VariableAndArray.py
@@ -2,5 +2,106 @@
 print "Content-type: text/html\n\n"
 
 age = 35
-
 print age
+
+print "<br>"
+
+pi = 3.14
+print pi
+
+print "<br>"
+
+name = 'Fikar'
+print name
+
+print "<br>"
+
+print age / pi
+print "<br>"
+print round(age / pi, 2)
+print "<br>"
+
+number = 5
+
+# String
+print name * age
+print '<br>'
+
+print age * pi
+print '<br>'
+
+print number * pi
+print "<br>"
+
+# The result is same as above
+print int(number) * pi
+print '<br>'
+
+print float(number) * pi
+print '<br>'
+
+
+'''String'''
+str = 'My names is '
+print str
+print '<br>'
+
+print str[0]
+print '<br>'
+
+print str[0:5]
+print '<br>'
+
+print  str[5]
+print '<br>'
+
+print str + name
+print '<br>'
+
+
+
+'''Array'''
+myList = ['fikar', 'fida', 'febrina','jagur']
+print myList
+print '<br>'
+
+print myList[1]
+print '<br>'
+
+print myList[2:4]
+print '<br>'
+
+
+myTuples = ('monyet', 'anjing', 'kucing')
+print myTuples
+print '<br>'
+
+print myTuples[1]
+# myTuples[1] = 'gajah'
+# print myTuples
+print '<br>'
+
+myList[1] ='indira'
+print myList
+print '<br>'
+
+
+
+'''Two dimensional array'''
+dict = {}
+dict['dad'] = 'firmantoko'
+dict['mom'] = 'indira'
+dict[1] = 'fikar'
+dict[2] = 'ani'
+
+print dict
+print '<br>'
+
+print dict['mom']
+print '<br>'
+
+'''Dictionary modules'''
+print dict.keys()
+print dict.values()
+
+
