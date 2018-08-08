aaa475f14c26d4a301a4e8a537eb3c6b8370d8f8
gekif <dzulfikar.maulana@gmail.com>
2018-08-08 18:41:43 +0700

    Login-Logout - Display Basic (FINAL)

--- 13-FinalProject/functions.php
+++ 13-FinalProject/functions.php
@@ -9,6 +9,7 @@ const DATABASE = 'finalprojectccrp';
 
 $link = mysqli_connect(HOST, USERNAME, PASSWORD, DATABASE);
 
+
 if (mysqli_connect_errno()) {
     print_r(mysqli_connect_errno());
     exit();
@@ -16,4 +17,85 @@ if (mysqli_connect_errno()) {
 
 if (isset($_GET['function']) == 'logout') {
     session_unset();
+}
+
+
+function time_since($since) {
+    $chunks = array(
+        array(60 * 60 * 24 * 365 , 'year'),
+        array(60 * 60 * 24 * 30 , 'month'),
+        array(60 * 60 * 24 * 7, 'week'),
+        array(60 * 60 * 24 , 'day'),
+        array(60 * 60 , 'hour'),
+        array(60 , 'min'),
+        array(1 , 's')
+    );
+
+    for ($i = 0, $j = count($chunks); $i < $j; $i++) {
+        $seconds = $chunks[$i][0];
+        $name = $chunks[$i][1];
+        if (($count = floor($since / $seconds)) != 0) {
+            break;
+        }
+    }
+
+    $print = ($count == 1) ? '1 '.$name : "$count {$name}s";
+    return $print;
+}
+
+
+function displayTweets($type){
+    global $link;
+
+    if ($type == 'public') {
+        $whereClause = "";
+    }
+
+    $query = "SELECT * FROM tweets" . $whereClause . " ORDER BY datetime DESC LIMIT 1";
+    $result = mysqli_query($link, $query);
+
+//    if ($result || mysqli_num_rows($result) == 0) {
+    if (mysqli_num_rows($result) == 0) {
+        echo "There are no tweets to display";
+
+    } else {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $userQuery = "SELECT * FROM users WHERE id = " . mysqli_real_escape_string($link, $row['userid']) .  " LIMIT 1";
+            $userQueryResult = mysqli_query($link, $userQuery);
+            $user = mysqli_fetch_assoc($userQueryResult);
+
+//            print_r($user);
+
+//            echo "<p>" . isset($user['email']) . "</p>";
+//            echo "<p>" . $user['email'] . time_since(time() - strtotime($row['datetime'])) . "</p>";
+//            echo "<p>" . $user['email'] . " <span class='time'> " . time_since(time() + strtotime($row['datetime'])) . " ago</span></p>";
+//            echo "<p>" . $user['email'] . " <span class='time'> " . time_since(strtotime($row['datetime'])) . " ago</span></p>";
+            echo "<div class='tweet'><p>" . $user['email'] . " <span class='time'> " . time_since(time() - strtotime($row['datetime'])) . " ago</span></p>";
+            echo "<p>" . $row['tweet'] . "</p>";
+//            echo "<p><a class='toggleFollow' data-userId='" . $row['userid'] . "'>Follow</a></p></div>";
+            echo "<p>Follow</p></div>";
+        }
+    }
+}
+
+
+function displaySearch() {
+    echo '<form class="form-inline">
+        <div class="form-group">
+          <input type="text" class="form-control mb-2 mr-sm-2" id="search" placeholder="Search">
+        </div>
+          <button type="submit" class="btn btn-primary mb-2">Cari Twit</button>
+        </form>';
+}
+
+
+function displayTweetBox() {
+    if (isset($_SESSION['id']) > 0) {
+        echo '<form class="form">
+            <div class="form-group">
+              <textarea class="form-control mb-2 mr-sm-2" id="tweetContent"></textarea>
+            </div>
+              <button type="submit" class="btn btn-primary mb-2">Post Twit</button>
+            </form>';
+    }
 }
--- 13-FinalProject/views/home.php
+++ 13-FinalProject/views/home.php
@@ -1,3 +1,17 @@
-<div class="container">
-    <h1>Home Page</h1>
+<div class="container mainContainer">
+
+    <div class="row">
+
+        <div class="col-8">
+            <h2>Recent Twit</h2>
+            <?php displayTweets('public'); ?>
+        </div>
+
+        <div class="col-4">
+            <?php displaySearch(); ?>
+            <hr>
+            <?php displayTweetBox(); ?>
+        </div>
+    </div>
+
 </div>
